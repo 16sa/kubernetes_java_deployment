59fc362f9100f08d723449644eb5253882e90289
16sa <safach2013@gmail.com>
2025-09-23 17:16:26 +0100

    update files

--- shopfront/src/test/java/uk/co/danielbryant/djshopping/shopfront/ShopfrontApplicationIT.java
+++ shopfront/src/test/java/uk/co/danielbryant/djshopping/shopfront/ShopfrontApplicationIT.java
@@ -1,15 +1,18 @@
 package uk.co.danielbryant.djshopping.shopfront;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = ShopfrontApplication.class)
 @TestPropertySource("classpath:application-test.properties")
 public class ShopfrontApplicationIT {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
         // This test ensures that the Spring application context loads successfully
         // Dummy properties from application-test.properties prevent failures in CI
     }
