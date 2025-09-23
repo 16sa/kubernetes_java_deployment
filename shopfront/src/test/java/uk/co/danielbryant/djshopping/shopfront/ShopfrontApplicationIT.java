9e53a5f21af8a842bfa35a0b8edd4f3bbc8b2d56
16sa <safach2013@gmail.com>
2025-09-23 17:13:05 +0100

    update files

--- shopfront/src/test/java/uk/co/danielbryant/djshopping/shopfront/ShopfrontApplicationIT.java
+++ shopfront/src/test/java/uk/co/danielbryant/djshopping/shopfront/ShopfrontApplicationIT.java
@@ -1,19 +1,16 @@
 package uk.co.danielbryant.djshopping.shopfront;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.TestPropertySource;
 
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = ShopfrontApplication.class)
-@TestPropertySource(properties = "eureka.client.enabled=false")
+@SpringBootTest(classes = ShopfrontApplication.class)
+@TestPropertySource("classpath:application-test.properties")
 public class ShopfrontApplicationIT {
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
+        // This test ensures that the Spring application context loads successfully
+        // Dummy properties from application-test.properties prevent failures in CI
     }
-
 }
