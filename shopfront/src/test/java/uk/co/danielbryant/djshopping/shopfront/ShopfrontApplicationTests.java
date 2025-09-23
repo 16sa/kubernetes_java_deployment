8f167ac4e891581720df81b08ff87ec72e0b9bea
16sa <safach2013@gmail.com>
2025-09-23 16:41:10 +0100

    update files

--- shopfront/src/test/java/uk/co/danielbryant/djshopping/shopfront/ShopfrontApplicationTests.java
+++ shopfront/src/test/java/uk/co/danielbryant/djshopping/shopfront/ShopfrontApplicationTests.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.TestPropertySource;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = ShopfrontApplication.class)
