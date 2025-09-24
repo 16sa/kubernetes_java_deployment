93ba2210c04837abe661408c3021980fb7334e13
16sa <safach2013@gmail.com>
2025-09-24 14:34:55 +0100

    update files

--- stockmanager/src/test/java/uk/co/danielbryant/djshopping/stockmanager/StockManagerApplicationTests.java
+++ stockmanager/src/test/java/uk/co/danielbryant/djshopping/stockmanager/StockManagerApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = StockManagerApplication.class)
-public class ShopfrontApplicationTests {
+public class StockManagerApplicationTests {
 
     @Autowired
     private TestRestTemplate testRestTemplate;
