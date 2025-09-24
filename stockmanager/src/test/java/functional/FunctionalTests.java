d43660ab4d87a857db7f789b6ca9c05c8c014c24
16sa <safach2013@gmail.com>
2025-09-24 15:04:13 +0100

    update files

--- stockmanager/src/test/java/functional/FunctionalTests.java
+++ stockmanager/src/test/java/functional/FunctionalTests.java
@@ -5,10 +5,16 @@ import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(monochrome = true,
-        features = "classpath:features/",
-        plugin = "html:build/reports/cucumber",
-        glue = "functional")
+@CucumberOptions(
+    monochrome = true,
+    features = "classpath:features/",
+    plugin = {
+        "html:build/reports/cucumber/report.html",
+        "json:build/reports/cucumber/report.json"
+    },
+    glue = "functional"
+)
 public class FunctionalTests {
 
 }
+
