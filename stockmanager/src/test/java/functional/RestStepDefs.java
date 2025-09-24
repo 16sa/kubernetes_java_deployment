bd3980a87674691e6069a648d3ee78758634f38c
16sa <safach2013@gmail.com>
2025-09-24 14:17:26 +0100

    update files

--- stockmanager/src/test/java/functional/RestStepDefs.java
+++ stockmanager/src/test/java/functional/RestStepDefs.java
@@ -1,8 +1,8 @@
 package functional;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import cucumber.java.en.Given;
+import cucumber.java.en.Then;
+import cucumber.java.en.When;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
